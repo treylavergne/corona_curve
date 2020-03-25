40b936ab2cfdcfb3ee08b4f03068078c9c7bee1d
treylavergne <treylavergne@gmail.com>
2020-03-25 10:07:37 -0500

    Add launch iterator to fetch dates

--- lib/corona_curve/api.rb
+++ lib/corona_curve/api.rb
@@ -1,7 +1,16 @@
 class API
-    def self.get_upcoming_launches
+    @@all_launch_dates = []
+    def self.get_launch_dates
         response = HTTParty.get('https://launchlibrary.net/1.3/launch?next=5
         ')
+
+        #binding.pry
+        
+        @@all_launch_dates = response['launches'].map {|hash| hash['net']}
+    end
+
+    def self.all_launch_dates
+        @@all_launch_dates
     end
 
 end
