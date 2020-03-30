ae515c694e2a0210ab460a8b5fa3ceab6eeadfde
treylavergne <treylavergne@gmail.com>
2020-03-30 15:28:00 -0500

    Refactored intro method into welcome method

--- lib/corona_curve/cli.rb
+++ lib/corona_curve/cli.rb
@@ -2,22 +2,19 @@ class CLI
     
     def start
         self.welcome
-        self.intro
         self.get_launch_dates
-        self.display_launch_dates
-        #self.user_date_input
-        self.get_ship_name
+        # self.display_launch_dates
+        # self.user_date_input
+        # self.get_ship_name
     end
     
+
     def welcome
         puts ""
         puts "Welcome to the Corona Curve!"
         puts ""
         #puts "Insert spaceship text"
         sleep(2)
-    end
-
-    def intro
         puts "Does COVID-19 have you worried?"
         puts ""
         puts "Are you sick of being in fear?"
@@ -26,16 +23,21 @@ class CLI
 
         puts "Let's get you on your way!"
         sleep(2)
-    end
-
-    def get_launch_dates
-        #API.get_launch_dates - pull these from Launch @@all instead of API
-        
         puts ""
         puts ""
         puts "First things first, when are you wanting to leave?" 
         puts "Choose from one of the upcoming dates below:"
         puts ""
+    end
+
+    def get_launch_dates
+        #API.get_launch_dates - pull these from Launch @@all instead of API
+        Launch.all.each.with_index(1) do |date, index|
+            puts "#{index}. #{date}"
+            puts ""
+            
+            end
+
     end
 
     def display_launch_dates
