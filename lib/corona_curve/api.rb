b4f50a6670f8ede11e98a8b6e16bbf29873288a9
treylavergne <treylavergne@gmail.com>
2020-03-30 14:22:51 -0500

    Refactored multiple API methods to one call

--- lib/corona_curve/api.rb
+++ lib/corona_curve/api.rb
@@ -1,16 +1,27 @@
 class API
-    @@all_launch_dates = []
-    def self.get_launch_dates
+    def self.get_launch_data
         response = HTTParty.get('https://launchlibrary.net/1.3/launch?next=5
         ')
-
+        response['launches'].each {|list| Launch.new(list)}
         #binding.pry
         
-        @@all_launch_dates = response['launches'].map {|hash| hash['net']}
+        #@@all_launches = response['launches'].each {|list| list['net']}
     end
 
-    def self.all_launch_dates
-        @@all_launch_dates
-    end
+    # def get_launch_dates
+    #     @@all_launches = response['launches'].map {|list| list['net']}
+
+    # end
+
+    # def self.all_launches
+    #     @@all_launches
+    # end
+
+    # #def self.get_ship_name
+    #     response = HTTParty.get('https://launchlibrary.net/1.3/launch?next=5
+    #     ')
+    #     @@all_ships = response['launches'].map {|list| list['name']}
+    #     puts @@all_ships
+    # end
 
 end
