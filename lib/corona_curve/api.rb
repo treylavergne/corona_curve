aaeb20e0b355ccf1832c40ab363b4c46048d3499
treylavergne <treylavergne@gmail.com>
2020-04-01 18:50:01 -0500

    Modify API to create new Launch object

--- lib/corona_curve/api.rb
+++ lib/corona_curve/api.rb
@@ -1,27 +1,13 @@
 class API
+    
     def self.get_launch_data
         response = HTTParty.get('https://launchlibrary.net/1.3/launch?next=5
         ')
-        response['launches'].each {|list| Launch.new(list)}
-        #binding.pry
-        
-        #@@all_launches = response['launches'].each {|list| list['net']}
+        @@all_api_data = response['launches'].each {|data_point| Launch.new(data_point)}
     end
 
-    # def get_launch_dates
-    #     @@all_launches = response['launches'].map {|list| list['net']}
-
-    # end
-
-    # def self.all_launches
-    #     @@all_launches
-    # end
-
-    # #def self.get_ship_name
-    #     response = HTTParty.get('https://launchlibrary.net/1.3/launch?next=5
-    #     ')
-    #     @@all_ships = response['launches'].map {|list| list['name']}
-    #     puts @@all_ships
-    # end
+    def self.all_api_data
+        @@all_api_data
+    end
 
 end
