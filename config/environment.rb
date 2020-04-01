eda764c27ec263e7d94405234cd91725aba2179b
treylavergne <treylavergne@gmail.com>
2020-04-01 18:45:14 -0500

    Modify launches file name to launch.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,7 @@
 require 'httparty'
 require 'pry'
 
-
+require_relative "../lib/corona_curve/launch.rb"
 require_relative "../lib/corona_curve/api.rb"
 require_relative "../lib/corona_curve/cli.rb"
 require_relative "../lib/corona_curve/version.rb"
-require_relative "../lib/corona_curve/launch.rb"
