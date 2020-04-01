c7dfb36b48860755d07e1bafc1d1b9b03216a5e2
treylavergne <treylavergne@gmail.com>
2020-04-01 18:55:37 -0500

    Add api_data method to load in API data

--- lib/corona_curve/cli.rb
+++ lib/corona_curve/cli.rb
@@ -1,20 +1,39 @@
 class CLI
     
     def start
-        self.welcome
-        self.get_launch_dates
-        # self.display_launch_dates
-        # self.user_date_input
-        # self.get_ship_name
+        self.intro
+        self.get_api_data
+        self.display_launch_dates
+        self.get_user_input
+       
+       
     end
     
 
-    def welcome
-        puts ""
-        puts "Welcome to the Corona Curve!"
+    def intro
+
+        puts "3"
+        sleep(0.75)
+        puts "2"
+        sleep(0.75)
+        puts "1"
+        sleep(0.75)
+
+        puts "
+                                                  
+                            █░█░█ █▀▀ █░░ █▀▀ █▀█ █▀▄▀█ █▀▀   ▀█▀ █▀█   ▀█▀ █░█ █▀▀
+                            ▀▄▀▄▀ ██▄ █▄▄ █▄▄ █▄█ █░▀░█ ██▄   ░█░ █▄█   ░█░ █▀█ ██▄
+
+        ░█████╗░░█████╗░██████╗░░█████╗░███╗░░██╗░█████╗░  ░█████╗░██╗░░░██╗██████╗░██╗░░░██╗███████╗
+        ██╔══██╗██╔══██╗██╔══██╗██╔══██╗████╗░██║██╔══██╗  ██╔══██╗██║░░░██║██╔══██╗██║░░░██║██╔════╝
+        ██║░░╚═╝██║░░██║██████╔╝██║░░██║██╔██╗██║███████║  ██║░░╚═╝██║░░░██║██████╔╝╚██╗░██╔╝█████╗░░
+        ██║░░██╗██║░░██║██╔══██╗██║░░██║██║╚████║██╔══██║  ██║░░██╗██║░░░██║██╔══██╗░╚████╔╝░██╔══╝░░
+        ╚█████╔╝╚█████╔╝██║░░██║╚█████╔╝██║░╚███║██║░░██║  ╚█████╔╝╚██████╔╝██║░░██║░░╚██╔╝░░███████╗
+        ░╚════╝░░╚════╝░╚═╝░░╚═╝░╚════╝░╚═╝░░╚══╝╚═╝░░╚═╝  ░╚════╝░░╚═════╝░╚═╝░░╚═╝░░░╚═╝░░░╚══════╝"
+        
         puts ""
         #puts "Insert spaceship text"
-        sleep(2)
+        sleep(0)
         puts "Does COVID-19 have you worried?"
         puts ""
         puts "Are you sick of being in fear?"
@@ -22,36 +41,91 @@ class CLI
         puts "Well then we have GREAT NEWS! Here at the Corona Curve, we're offering expedited tickets to outer space all for the low price of 1 toilet paper roll. Forget about flattening the curve - we'll avoid the curve entirely!"
 
         puts "Let's get you on your way!"
-        sleep(2)
+        sleep(0)
         puts ""
         puts ""
         puts "First things first, when are you wanting to leave?" 
-        puts "Choose from one of the upcoming dates below:"
+        puts "Choose from one of the upcoming dates below or exit :"
         puts ""
+
+
     end
 
-    def get_launch_dates
-        #API.get_launch_dates - pull these from Launch @@all instead of API
-        Launch.all.each.with_index(1) do |date, index|
-            puts "#{index}. #{date}"
-            puts ""
-            
-            end
-
+    def get_api_data
+        API.get_launch_data
+        API.all_api_data
     end
 
     def display_launch_dates
-        API.all_launches.each.with_index(1) do |date, index|
-        puts "#{index}. #{date}"
-        puts ""
+        #API.get_launch_dates - pull these from Launch @@all instead of API
+        
+        Launch.launch_dates
+
+    end
+
+    def get_user_input
+        #and handle
+
+        input = nil
+            while input != 'exit'
+                input = gets.strip
+                
+                if input == "exit"
+                    goodbye
+                    break
+                # elsif 
+               elsif true # to validate user input
+                @launch_object = Launch.all[input.to_i - 1]
+                display_launch_object
+                puts "Would you like to see other launches or would you like to exit? Select another number or exit."
+                get_user_input
+
+
+            
+                else
+                    invalid_input
+                end
+           
         end
-        input = gets.chomp
-    end
-
-    def user_date_input
-        date_input = gets.chomp
+        
 
     end
+
+    def display_launch_object
+        puts "#{@launch_object.name}"
+        puts "#{@launch_object.api_id}"
+        puts "#{@launch_object.date}"
+    end
+
+    def invalid_input
+        puts "Did you mean to type that? Feel free to enter one of the selections or exit if you're through."
+        puts ""
+        display_launch_dates
+    end
+
+
+
+    def goodbye
+        puts "Thanks for visiting! #swervethcurve"
+        exit
+    end
+
+
+
+    
+
+    # def display_launch_dates
+    #     # API.get_launch_data.each.with_index(1) do |date, index|
+    #     # puts "#{index}. #{date}"
+    #     # puts ""
+    #     end
+    #     input = gets.chomp
+    # end
+
+    # def user_date_input
+    #     date_input = gets.chomp
+
+    # end
 
     #def get_ship_name
      #   API.get_ship_name
