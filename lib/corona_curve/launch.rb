759bd93fcf3a78b7dd5bc4be906cb5d73ebcdc39
treylavergne <treylavergne@gmail.com>
2020-04-01 18:47:05 -0500

    Modify attributes to match API names

--- lib/corona_curve/launch.rb
+++ lib/corona_curve/launch.rb
@@ -3,16 +3,29 @@ class Launch
     @@all = []
 
     def initialize(attributes)
-        binding.pry
         @api_id = attributes['id']
         @name = attributes['name']
         @date = attributes['net']
         @@all << self
+        
     end
 
     def self.all
         @@all
     end
+    
+    def self.launch_dates
+
+        @@all.each.with_index(1) do |object, index|
+            puts "#{index}. #{object.date}"
+            puts ""
+        end
+        # binding.pry
+    end
+
+    # def self.find_by_choice(input)
+    #     all.find{|input| all[input.to_i]}
+    # end
 
 
 end
