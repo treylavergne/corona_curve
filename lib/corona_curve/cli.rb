01ce85d815ef47e3a62b08f42f1e030026747143
treylavergne <treylavergne@gmail.com>
2020-04-03 20:40:12 -0500

    Touch up text format and colors

--- lib/corona_curve/cli.rb
+++ lib/corona_curve/cli.rb
@@ -1,12 +1,10 @@
 class CLI
     
     def start
-        # self.intro
+        self.intro
         self.get_api_data
         self.display_launch_dates
         self.get_user_input
-       
-       
     end
     
 
@@ -45,8 +43,9 @@ class CLI
         ██║░░╚═╝██║░░██║██████╔╝██║░░██║██╔██╗██║███████║  ██║░░╚═╝██║░░░██║██████╔╝╚██╗░██╔╝█████╗░░
         ██║░░██╗██║░░██║██╔══██╗██║░░██║██║╚████║██╔══██║  ██║░░██╗██║░░░██║██╔══██╗░╚████╔╝░██╔══╝░░
         ╚█████╔╝╚█████╔╝██║░░██║╚█████╔╝██║░╚███║██║░░██║  ╚█████╔╝╚██████╔╝██║░░██║░░╚██╔╝░░███████╗
-        ░╚════╝░░╚════╝░╚═╝░░╚═╝░╚════╝░╚═╝░░╚══╝╚═╝░░╚═╝  ░╚════╝░░╚═════╝░╚═╝░░╚═╝░░░╚═╝░░░╚══════╝"
+        ░╚════╝░░╚════╝░╚═╝░░╚═╝░╚════╝░╚═╝░░╚══╝╚═╝░░╚═╝  ░╚════╝░░╚═════╝░╚═╝░░╚═╝░░░╚═╝░░░╚══════╝".green
         
+        sleep(2)
         puts ""
         puts ""
         sleep(1.75)
@@ -56,20 +55,21 @@ class CLI
         puts "Are you sick of being in fear?"
         puts ""
         sleep(2)
-        puts "Well then we have GREAT NEWS! Here at the Corona Curve,"
+        puts "Well then we have GREAT NEWS! Here at the \e[4mCorona Curve\e[0m,"
         puts "we want to help you out by giving you the most up-to-date info on "
-        puts "upcoming trips to outer space."
-        sleep(4)
-        puts "Forget about flattening the curve - we'll avoid the curve entirely!"
-        sleep(3)
+        puts "upcoming trips to outer space so that you can plan accordingly."
+        sleep(6.50)
+        puts "Forget about \e[3mflattening the curve\e[0m, with us - \e[1myou can avoid the curve entirely!\e[0m"
+        sleep(5)
         puts "Let's get you on your way!"
         sleep(4)
         puts ""
         puts ""
-        puts "First things first, when are you wanting to leave?" 
-        puts "Choose from one of the upcoming dates by typing it's number below or type exit to leave the program:"
+        puts "First things first, when are you wanting to leave?"
+        sleep(3)
+        puts "Choose from one of the upcoming dates by \e[1mentering it's number below\e[0m or typing \e[1mexit\e[0m to leave the program:"
         puts ""
-        sleep(4)
+        sleep(5)
 
     end
 
@@ -84,14 +84,13 @@ class CLI
     end
 
     def get_user_input
-        
         input = nil
             while input = gets.strip
                 if input.to_i.between?(1, 5) # to validate user input
                 @launch_object = Launch.all[input.to_i - 1]
                 display_launch_object
                 puts ""
-                puts "Would you like to see other launches or would you like to exit? Select another number or type exit to leave."
+                puts "Would you like to see other launches or would you like to exit? \e[1mSelect another number\e[0m or type \e[1mexit\e[0m to leave."
                 get_user_input
 
                 elsif input == 'exit'
@@ -101,49 +100,30 @@ class CLI
                     invalid_input
                 end
             end
-           
-                
-            
-        
     end
 
     def display_launch_object
         puts ""
         puts "This launch leaves #{@launch_object.date} "
         puts "and the rocket's name is the #{@launch_object.name}."
-        puts "This id number for this launch is #{@launch_object.api_id} in case you need to reference it to your ticket holder."
+        puts "This id number for this launch is #{@launch_object.api_id} in case you need to reference it to your \e[1mticket holder\e[0m."
         sleep(1)
     end
 
     def invalid_input
         puts ""
-        puts "Did you mean to type that? Feel free to type in one of the selections provided or type exit if you're done."
+        puts "\e[1mDid you mean to type that?\e[0m Feel free to \e[1mtype in one of the selections provided\e[0m or type \e[1mexit\e[0m if you're done."
         puts ""
         display_launch_dates
     end
 
     def goodbye
         puts ""
-        puts "Thanks for visiting and stay safe! #swervethcurve"
+        puts ""
+        puts "Thanks for visiting and stay safe! \e[1m#swervethcurve\e[0m"
+        puts ""
         puts ""
         exit
     end
-
-
-    
-
-    # def display_launch_dates
-    #     # API.get_launch_data.each.with_index(1) do |date, index|
-    #     # puts "#{index}. #{date}"
-    #     # puts ""
-    #     end
-    #     input = gets.chomp
-    # end
-
- 
-
-
-
-
 
 end
